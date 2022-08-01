edb21fbda86dfc0ef464cb1c8351e04e29c08299
Limjw <vvovovv18@gmail.com>
2022-08-01 17:46:45 +0900

    2022-08-01

---
+++ javastudy/06_INHERITANCE/src/quiz01_coffee/Coffee.java
@@ -0,0 +1,15 @@
+package quiz01_coffee;
+
+public class Coffee {
+	
+	private String origin;
+
+	public Coffee(String origin) {
+		super();
+		this.origin = origin;
+	}
+	public void info() {
+		System.out.print(origin);
+
+	}
+}
---
+++ javastudy/06_INHERITANCE/src/quiz01_coffee/Espresso.java
@@ -0,0 +1,17 @@
+package quiz01_coffee;
+
+public class Espresso extends Coffee{
+	
+	private int water;
+
+	public Espresso(String origin, int water) {
+		super(origin);
+		this.water = water;
+	}
+	@Override
+	public void info() {
+		super.info();
+		System.out.print(", 물 " + water + "ml ");
+	}
+
+}
