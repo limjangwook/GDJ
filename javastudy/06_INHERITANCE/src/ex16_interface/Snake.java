24dbfec313da54cb1dfabd643de2ad35b2c68007
Limjw <vvovovv18@gmail.com>
2022-08-03 17:27:09 +0900

    2022-08-03

---
+++ javastudy/06_INHERITANCE/src/ex16_interface/Snake.java
@@ -0,0 +1,12 @@
+package ex16_interface;
+
+//extends Pet : 애완동물이다.
+
+public class Snake extends Pet {
+
+	public Snake(String petName) {
+		super(petName);
+		
+	}
+
+}
---
+++ javastudy/06_INHERITANCE/src/quiz08_schedule/Main.java
@@ -0,0 +1,12 @@
+package quiz08_schedule;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		WeekScheduler schedular = new WeekScheduler(38);
+		schedular.manage();
+
+	}
+
+}
