70e2abaa2be71e7dfccbdc0f0beb7803a246aad4
Limjw <vvovovv18@gmail.com>
2022-08-05 17:51:16 +0900

    2022-08-05

---
+++ javastudy/08_EXCEPTION/src/quiz04_employee/Temporary.java
@@ -0,0 +1,38 @@
+package quiz04_employee;
+
+public class Temporary extends Employee {
+	
+	private double hourPay;
+	private int workTimes;
+	
+	public Temporary(int empNo, String name) {
+		super(empNo, name);
+	}
+
+	public double getHourPay() {
+		return hourPay;
+	}
+
+	public void setHourPay(double hourPay) {
+		this.hourPay = hourPay;
+	}
+
+	public int getWorkTimes() {
+		return workTimes;
+	}
+
+	public void setWorkTimes(int workTimes) {
+		this.workTimes = workTimes;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", 녹봉=" + hourPay + ", 일하는 시간" + workTimes + "]";
+	}
+	
+	@Override
+	public int getPay() {
+		return (int)(hourPay * workTimes);
+	}
+
+}
--- javastudy/ParkinLotProject/src/parking/ParkingLot.java
+++ javastudy/ParkinLotProject/src/parking/ParkingLot.java
@@ -1,5 +1,6 @@
 package parking;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -19,8 +20,7 @@ public class ParkingLot {
 	
 	public void addCar() {
 		if(idx == cars.length) {
-			System.out.println("더 이상 차량 등록이 불가능합니다.");
-			return;
+			throw new RuntimeException("더 이상 차량 등록이 불가능합니다.");
 		}
 		System.out.println("전체 등록된 차량" + (idx) + "대");
 		System.out.print("차량번호 >>> ");
@@ -29,12 +29,12 @@ public class ParkingLot {
 		String model = sc.next();
 		Car car = new Car(carNo, model);
 		cars[idx++] = car;
+		System.out.println("차량번호" + carNo + "차량이 등록되었습니다.");
 	}
 	
 	public void deleteCar() {
 		if(idx == 0) {
-			System.out.println("등록된 차량이 없습니다. ");
-			return;
+			throw new RuntimeException("등록된 차량이 없습니다.");
 		}
 		System.out.print("제거할 차량번호 >>> ");
 		String carDel = sc.next();
@@ -52,8 +52,7 @@ public class ParkingLot {
 	
 	public void printAllCars() {
 		if(idx == 0) {
-			System.out.println("등록된 차량이 없습니다.");
-			return;
+			throw new RuntimeException("등록된 차량이 없습니다.");
 		}
 		System.out.println(name + " 차량 목록");
 		for(int i = 0; i < idx; i++) {
@@ -63,16 +62,23 @@ public class ParkingLot {
 	
 	public void manage() {
 		while(true) {
-			System.out.print("1.추가 2.삭제 3.전체 0.종료 >>> ");
-			int choice = sc.nextInt();
-			sc.nextLine();
-			switch(choice) {
-			case 1: addCar(); break;
-			case 2: deleteCar(); break;
-			case 3: printAllCars();break;
-			case 0: System.out.println("ParkingLot 프로그램을 종료합니다.");
-					return;
-			default: System.out.println("존재하지 않는 메뉴입니다.");
+			try {
+				System.out.print("1.추가 2.삭제 3.전체 0.종료 >>> ");
+				int choice = sc.nextInt();
+				sc.nextLine();
+				switch(choice) {
+				case 1: addCar(); break;
+				case 2: deleteCar(); break;
+				case 3: printAllCars();break;
+				case 0: System.out.println( name +"을 이용해주셔서 감사합니다.");
+						return;
+				default: System.out.println("존재하지 않는 메뉴입니다.");
+				}
+			} catch(InputMismatchException e) {
+				sc.next();
+				System.out.println("다시 눌러주십시오.");
+			} catch(RuntimeException e) {
+				System.out.println(e.getMessage());
 			}
 		}
 	}
