128b9f3d6776d59b5e02c25695400f736d7cc9e7
LIMJW <vvovovv18@gmail.com>
2022-10-12 17:28:01 +0900

    2022-10-12

--- webstudy/UI/login.js
+++ webstudy/UI/login.js
@@ -2,27 +2,12 @@
 
 document.getElementById('btn_signin').onclick = function(event){
     var pw = document.getElementById('pw');
-    var id = document.getElementById('id');
-    if(id.value == '') {
-        alert('ID를 입력하세요.');
-        event.preventDefault();
-        return;
-    }
-    if(id.value.length < 4) {
-        alert('ID는 4자 이상 입력하세요.');
-        event.preventDefault();
-        return;
-    }
     if(pw.value == '') {
         alert('비밀번호를 입력하세요.');
         event.preventDefault();
         return;
     }
-    if(pw.value.length < 4) {
-        alert('비밀번호를 4자 이상 입력하세요.');
-        event.preventDefault();
-        return;
-    }
+    
 }
 document.getElementById('id').onkeyup = function(event){
     var id = document.getElementById('id');
@@ -31,8 +16,13 @@ document.getElementById('id').onkeyup = function(event){
         id_msg.textContent = '';
     } else if(id.value.length < 4) {
         id_msg.textContent = '아이디는 4자 이상입니다.';
+        id_msg.style.color = '#FF0000';
+        id_msg.style.fontSize = '14px';
     } else if(id.value.length >= 4) {
         id_msg.textContent = '정상적인 아이디입니다.';
+        id_msg.style.color = '#0000FF';
+        id_msg.style.fontSize = '14px';
+
     }
 }
 
