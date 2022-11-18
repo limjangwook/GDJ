9cb630ddb32bd0c13305341ca2956347543d096d
LIMJW <vvovovv18@gmail.com>
2022-11-18 16:33:51 +0900

    2022-11-18

--- springstudy/13_Pjt_User/src/main/java/com/gdu/app13/mapper/UserMapper.java
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.gdu.app13.domain.RetireUserDTO;
+import com.gdu.app13.domain.SleepUserDTO;
 import com.gdu.app13.domain.UserDTO;
 
 @Mapper
@@ -23,5 +24,10 @@ public interface UserMapper {
 	public int insertRetireUser(RetireUserDTO retireUser);
 	public int updateSessionInfo(UserDTO user);
 	public int updateUserPassword(UserDTO user);
+	public int insertSleepUser();
+	public int deleteUserForSleep();
+	public SleepUserDTO selectSleepUserById(String id);
+	public int insertRestoreUser(String id);
+	public int deleteSleepUser(String id);
 	
 }
