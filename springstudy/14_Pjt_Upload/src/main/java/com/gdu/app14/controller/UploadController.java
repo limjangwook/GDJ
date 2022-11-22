26a221681f1ce3622136419a5b0fbfb6b4c4af30
LIMJW <vvovovv18@gmail.com>
2022-11-22 15:23:50 +0900

    2022-11-22

--- springstudy/14_Pjt_Upload/src/main/java/com/gdu/app14/controller/UploadController.java
+++ springstudy/14_Pjt_Upload/src/main/java/com/gdu/app14/controller/UploadController.java
@@ -3,10 +3,15 @@ package com.gdu.app14.controller;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.gdu.app14.service.UploadService;
@@ -36,6 +41,24 @@ public class UploadController {
 	@PostMapping("/upload/add")
 	public void add(MultipartHttpServletRequest multipartRequest, HttpServletResponse response) {
 		uploadService.save(multipartRequest, response);
+	}
+	
+	@GetMapping("/upload/detail")
+	public String detail(@RequestParam(value = "uploadNo", required=false, defaultValue="0") int uploadNo, Model model) {
+		uploadService.getUploadByNo(uploadNo, model);
+		return "upload/detail";
+	}
+	
+	@ResponseBody
+	@GetMapping("/upload/download")
+	public ResponseEntity<Resource> download(@RequestHeader("User-Agent") String userAgent, int attachNo) {
+		return uploadService.download(userAgent, attachNo);
+	}
+	
+	@GetMapping("/upload/attach/remove")
+	public String attachRemove(@RequestParam("uploadNo") int uploadNo, @RequestParam("attachNo") int attachNo) {
+		uploadService.removeAttachByAttachNo(attachNo);
+		return "redirect:/upload/detail?uploadNo=" + uploadNo;
 	}
 	
 }
