2d9cb9126994652aac2208bf90dfccd858722282
LIMJW <vvovovv18@gmail.com>
2022-12-07 20:16:23 +0900

    2022-12-07

---
+++ springstudy/REST/src/main/java/com/gdu/rest/service/MemberServiceImpl.java
@@ -0,0 +1,37 @@
+package com.gdu.rest.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gdu.rest.domain.MemberDTO;
+import com.gdu.rest.mapper.MemberMapper;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	@Override
+	public Map<String, Object> register(MemberDTO member, HttpServletResponse response) {
+
+		try {
+			
+			Map<String, Object> result = new HashMap<String, Object>();
+			result.put("inserResult", memberMapper.insertMember(member));
+			return result;
+			
+		} catch(Exception e) {
+			
+			System.out.println(e.getClass().getName());
+			
+		}
+		
+		return null;
+	}
+}
